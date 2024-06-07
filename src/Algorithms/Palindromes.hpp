34edb691c8f195290591ff2020225c707deaa738
Tolik708 <illia.vidanov@gmail.com>
2024-06-07 13:16:41 +0300

    Update Palindromes.hpp
    Finally working Palindromes library that I am happy with

--- src/Algorithms/Palindromes.hpp
+++ src/Algorithms/Palindromes.hpp
@@ -1,5 +1,5 @@
-#ifndef TOLIK_ALGORITHMS_POLINDROMES_HPP
-#define TOLIK_ALGORITHMS_POLINDROMES_HPP
+#ifndef TOLIK_ALGORITHMS_PALINDROMES_HPP
+#define TOLIK_ALGORITHMS_PALINDROMES_HPP
 
 #include <functional>
 #include <utility>
@@ -13,15 +13,25 @@
 
 namespace Tolik
 {
-namespace polindrome
+namespace palindrome
 {
-template<typename T = uint8_t>
-struct DigitRange
+template<typename T>
+struct DigitCountRange
 {
-	constexpr DigitRange() {}
-	constexpr DigitRange(T min, T max) : min(min), max(max) {}
+public:
+	constexpr DigitCountRange() {}
+	constexpr DigitCountRange(T newMin, T newMax) : min(newMin), max(newMax) {}
 	T min = 0;
 	T max = 0;
+};
+
+struct DigitRange
+{
+public:
+	constexpr DigitRange() {}
+	constexpr DigitRange(uint8_t newMin, uint8_t newMax) : min(newMin), max(newMax) {}
+	uint8_t min = 0;
+	uint8_t max = 0;
 };
 
 // Get all palindromes possible fitting in this data type
@@ -30,7 +40,7 @@ constexpr inline void GetAllPalindromes(Functor callback);
 
 // Get polindromse in digit range first - includive, second - exclusive
 template<typename T, typename Functor>
-constexpr inline void GetPalindromesDigitCount(const DigitRange<T> &digitCountRange, Functor callback);
+constexpr inline void GetPalindromesDigitCountRange(const DigitCountRange<T> &digitCountRange, Functor callback);
 
 // Get palindromes whose digits doesn't overflow set ranges { {2, 6}, {3, 7}, {1, 8}, {2, 5} } = max 4664; min 2332
 // First number inclusive last exclusive
@@ -38,26 +48,26 @@ template<typename Functor>
 constexpr inline void GetPalindromesDigitRange(const std::vector<DigitRange> &ranges, Functor callback);
 
 //template<typename T>
-//constexpr bool IsPolindrome(T number);
+//constexpr bool IsPalindrome(T number);
 
 //template<typename T>
-//constexpr DefUIntType GetIndexOfPolindrome(T polindrome);
+//constexpr DefUIntType GetIndexOfPalindrome(T palindrome);
 
 //template<typename T = DefUIntType>
-//constexpr T GetPolindromeWithIndex(DefUIntType index);
+//constexpr T GetPalindromeWithIndex(DefUIntType index);
 
 //template<typename T = DefUIntType>
-//constexpr inline T GetPolindromeCountInNDigitNumber(DefUIntType numberDigits) 
+//constexpr inline T GetPalindromeCountInNDigitNumber(DefUIntType numberDigits) 
 //{ return 9 * gcem::pow(10, gcem::ceil(numberDigits / 2.0) - 1); }
 
 //template<typename T>
-//constexpr inline DefUIntType GetCountOfDigitsFromPolindromeIndex(T polindromeIndex)
-//{ return gcem::ceil(2 * gcem::log10(polindromeIndex / 9.0) + 2); }
+//constexpr inline DefUIntType GetCountOfDigitsFromPalindromeIndex(T palindromeIndex)
+//{ return gcem::ceil(2 * gcem::log10(palindromeIndex / 9.0) + 2); }
 
 
 namespace detail
 {
-// Same as GetPalindromes(const std::vector<DigitRange> &ranges) but with valid ranges, that means half the amount of ranges that polindrome needs
+// Same as GetPalindromes(const std::vector<DigitRange> &ranges) but with valid ranges, that means half the amount of ranges that palindrome needs
 template<typename Functor>
 constexpr inline void GetPalindromesFromValidRanges(const std::vector<DigitRange> &validRanges, bool odd, Functor callback);
 
@@ -66,20 +76,30 @@ constexpr inline void GetPalindromesFromValidRanges(const std::vector<DigitRange
 // In this case min = 0
 // Example: 235 = { {0, 3}, {0, 4}, {0, 6} }
 template<typename T>
-constexpr inline auto GetValidRangesFromNumber(T max) -> std::vector<DigitRange>;
+#if __cplusplus >= 202002L
+constexpr
+#endif
+inline auto GetValidRangesFromNumber(T max) -> std::vector<DigitRange>;
 
 // Create valid ranges from given numbers.
 // Both values are inclusive
 // Example: (121, 235) { {1, 3}, {2, 4}, {1, 6} }
 template<typename T, typename U>
-constexpr inline auto GetValidRangesFromNumber(T min, U max) -> std::vector<DigitRange>;
+#if __cplusplus >= 202002L
+constexpr
+#endif
+inline auto GetValidRangesFromNumber(T min, U max) -> std::vector<DigitRange>;
 
-// Convert ranges from full to half size, because polindrome needs only them
+// Convert ranges from full to half size, because palindrome needs only them
 // Example: { {1, 3}, {2, 4}, {1, 6}, {2, 3}, {2, 3} } = { {2, 3}, {2, 3}, {1, 6} }
 // Same as:
 // for(std::size_t i = 0; i < validRangesCount; i++)
 //	validRanges[i] = DigitRange(std::max(ranges[i].min, ranges[ranges.size() - i - 1].min), std::min(ranges[i].max, ranges[ranges.size() - i - 1].max));
-constexpr inline auto GetValidRanges(const std::vector<DigitRange> &ranges) -> std::vector<DigitRange>;
+// Can be constexpr only from c++20 where constexpr memory allocation is allowed
+#if __cplusplus >= 202002L
+constexpr
+#endif
+inline auto GetValidRanges(const std::vector<DigitRange> &ranges) -> std::vector<DigitRange>;
 
 template<typename ReturnType, typename U, typename Functor>
 constexpr void IteratePalindromes(ReturnType &number, U digit, U totalDigits, Functor callback);
@@ -92,11 +112,26 @@ constexpr void IteratePalindromes(ReturnType &number, U digit, U totalDigits, Fu
 template<typename ReturnType, typename U, typename Functor>
 constexpr void IteratePalindromesRanging(ReturnType &number, U digit, U totalDigits, const std::vector<DigitRange> &validRanges, Functor callback);
 
-// Get needed difference to get next polindrome
+
+template<typename T>
+constexpr static T kGetDiffLookup[21] =
+{
+	0, 1, 11, 101, kMaxDigits<T> > 3 ? 1001 : 0,
+    kMaxDigits<T> > 4 ?  (gcem::pow(10, 4) + 1) : 0,  kMaxDigits<T> > 5 ?  (gcem::pow(10, 5) + 1) : 0,  kMaxDigits<T> > 6 ?  (gcem::pow(10, 6) + 1) : 0,  kMaxDigits<T> > 7 ?  (gcem::pow(10, 7) + 1) : 0,
+    kMaxDigits<T> > 8 ?  (gcem::pow(10, 8) + 1) : 0,  kMaxDigits<T> > 9 ?  (gcem::pow(10, 9) + 1) : 0,  kMaxDigits<T> > 10 ? (gcem::pow(10, 10) + 1) : 0, kMaxDigits<T> > 11 ? (gcem::pow(10, 11) + 1) : 0, 
+    kMaxDigits<T> > 12 ? (gcem::pow(10, 12) + 1) : 0, kMaxDigits<T> > 13 ? (gcem::pow(10, 13) + 1) : 0, kMaxDigits<T> > 14 ? (gcem::pow(10, 14) + 1) : 0, kMaxDigits<T> > 15 ? (gcem::pow(10, 15) + 1) : 0,
+    kMaxDigits<T> > 16 ? (gcem::pow(10, 16) + 1) : 0, kMaxDigits<T> > 17 ? (gcem::pow(10, 17) + 1) : 0, kMaxDigits<T> > 18 ? (gcem::pow(10, 18) + 1) : 0, kMaxDigits<T> > 19 ? (gcem::pow(10, 19) + 1) : 0
+};
+
+// Get needed difference to get next palindrome
 // totalDigits is used to add offset
 // same as: (10^digit + 1) * (totalDigits - digit) / 2;
 template<typename T, typename U>
-constexpr inline T GetDiff(U digit, U totalDigits);
+// static variables in constepxr functions are allowed only from c++20
+#if __cplusplus >= 202002L
+constexpr
+#endif
+inline T GetDiff(U digit, U totalDigits);
 } // detail
 
 
@@ -116,27 +151,24 @@ constexpr void GetAllPalindromes(Functor callback)
         number = 0;
 	}
 
-	detail::IteratePalindromesRanging(number, std::numeric_limits<ReturnType>::digits10 + 1, std::numeric_limits<ReturnType>::digits10 + 1, GetValidRangesFromNumber(std::numeric_limits<ReturnType>::max()), callback);
+	detail::IteratePalindromesRanging(number, std::numeric_limits<ReturnType>::digits10 + 1, std::numeric_limits<ReturnType>::digits10 + 1, detail::GetValidRangesFromNumber(std::numeric_limits<ReturnType>::max()), callback);
 }
 
 template<typename T, typename Functor>
-constexpr void GetPalindromesDigitCount(const DigitRange<T> &digitCountRange, Functor callback)
+constexpr void GetPalindromesDigitCountRange(const DigitCountRange<T> &digitCountRange, Functor callback)
 {
     using ReturnType = typename FunctorTraits<Functor>::arg<0>::type;
     ReturnType number = ReturnType(0);
-	if(0 < digitRange.first && 1 < digitRange.second)
+	if(digitCountRange.min < 2 && 1 < digitCountRange.max)
 		callback(number);
 
-	// With c++20 I could have written std::is_constant_evaluated to avoid logging in constexpr variant
-    if(std::numeric_limits<ReturnType>::is_specialized && std::numeric_limits<ReturnType>::digits10 < digitRange.second)
+	// With c++20 I could have written std::is_constant_evaluated to avoid calling non constexpr function in constexpr variant
+    if(std::numeric_limits<ReturnType>::is_specialized && std::numeric_limits<ReturnType>::digits10 < digitCountRange.max)
         std::cout << "Number might overflow"; // TODO: Use logger
     
-    bool odd = true;
-    for(T digit = digitRange.first; digit < digitRange.second; digit = digit + U(1))
+    for(T digit = digitCountRange.min; digit < digitCountRange.max; digit = digit + T(1))
     {
-        IteratePalindromes(number, digit, digit, odd, callback);
-        
-        odd = !odd;
+        detail::IteratePalindromes(number, digit, digit, callback);
         number = 0;
     }
 }
@@ -149,8 +181,9 @@ constexpr void GetPalindromesDigitRange(const std::vector<DigitRange> &ranges, F
 	if(ranges.size() == 1)
 		callback(number);
 
-	IteratePalindromesRanging(number, ranges.size(), ranges.size(), GetValidRanges(ranges), callback);
+	detail::IteratePalindromesRanging(number, ranges.size(), ranges.size(), detail::GetValidRanges(ranges), callback);
 }
+
 
 
 namespace detail
@@ -167,66 +200,56 @@ constexpr void GetPalindromesFromValidRanges(const std::vector<DigitRange> &rang
 	IteratePalindromesRanging(number, totalDigits, totalDigits, ranges, callback);
 }
 
+
 template<typename T>
+#if __cplusplus >= 202002L
+constexpr
+#endif
 auto GetValidRangesFromNumber(T max) -> std::vector<DigitRange>
 {
 	const std::size_t n = NumberDigits(max);
 	const std::size_t validRangesCount = (n + 1) / 2;
-	ranges_t validRanges(validRangesCount);
+	std::vector<DigitRange> validRanges(validRangesCount);
 
 	for(std::size_t i = 0; i < validRangesCount; i++)
-		validRanges[i] = std::pair<uint8_t, uint8_t>(0, std::min(GetDigit(max, i), GetDigit(max, n - i - 1)) + 1);
+		validRanges[i] = DigitRange(0, std::min(GetDigit(max, i), GetDigit(max, n - i - 1)) + 1);
 
 	return validRanges;
 }
 
 template<typename T, typename U>
-constexpr T GetDiff(U digit, U totalDigits)
-{
-	if constexpr(!std::is_arithmetic_v<T>)
-        return (IntegralPower(T(10), digit) + T(1)) * (totalDigits - digit) / U(2);
-    else
-    {
-        static const uint32_t maxDigits = std::numeric_limits<T>::digits10 + 1;
-        static const T lookup[21] = { 0, 1, 11, 101, maxDigits > 3 ? 1001 : 0,
-                                      maxDigits > 4 ? (gcem::pow(10, 4) + 1) : 0,   maxDigits > 5 ? (gcem::pow(10, 5) + 1) : 0,   maxDigits > 6 ? (gcem::pow(10, 6) + 1) : 0,   maxDigits > 7 ? (gcem::pow(10, 7) + 1) : 0,
-                                      maxDigits > 8 ? (gcem::pow(10, 8) + 1) : 0,   maxDigits > 9 ? (gcem::pow(10, 9) + 1) : 0,   maxDigits > 10 ? (gcem::pow(10, 10) + 1) : 0, maxDigits > 11 ? (gcem::pow(10, 11) + 1) : 0, 
-                                      maxDigits > 12 ? (gcem::pow(10, 12) + 1) : 0, maxDigits > 13 ? (gcem::pow(10, 13) + 1) : 0, maxDigits > 14 ? (gcem::pow(10, 14) + 1) : 0, maxDigits > 15 ? (gcem::pow(10, 15) + 1) : 0,
-                                      maxDigits > 16 ? (gcem::pow(10, 16) + 1) : 0, maxDigits > 17 ? (gcem::pow(10, 17) + 1) : 0, maxDigits > 18 ? (gcem::pow(10, 18) + 1) : 0, maxDigits > 19 ? (gcem::pow(10, 19) + 1) : 0 };
-        return lookup[digit] * std::pow(T(10), (totalDigits - digit) / U(2));
-    }
-}
-} // detail
-} // polindrome
-
-
-
-template<typename T, typename U>
-Palindromes::ranges_t Palindromes::GetValidRangesFromNumber(T min, U max)
+#if __cplusplus >= 202002L
+constexpr
+#endif
+auto GetValidRangesFromNumber(T min, U max) -> std::vector<DigitRange>
 {
 	const std::size_t n = NumberDigits(max);
 	const std::size_t validRangesCount = (n + 1) / 2;
-	ranges_t validRanges(validRangesCount);
+	std::vector<DigitRange> validRanges(validRangesCount);
 
 	for(std::size_t i = 0; i < validRangesCount; i++)
-		validRanges[i] = std::pair<uint8_t, uint8_t>(std::max(GetDigit(min, i), GetDigit(min, n - i - 1)), std::min(GetDigit(max, i), GetDigit(max, n - i - 1)) + 1);
+		validRanges[i] = DigitRange(std::max(GetDigit(min, i), GetDigit(min, n - i - 1)), std::min(GetDigit(max, i), GetDigit(max, n - i - 1)) + 1);
 
 	return validRanges;
 }
 
-Palindromes::ranges_t Palindromes::GetValidRanges(const ranges_t &ranges)
+#if __cplusplus >= 202002L
+constexpr
+#endif
+auto GetValidRanges(const std::vector<DigitRange> &ranges) -> std::vector<DigitRange>
 {
 	const std::size_t validRangesCount = (ranges.size() + 1) / 2;
-	ranges_t validRanges(validRangesCount);
+	std::vector<DigitRange> validRanges(validRangesCount);
 
 	for(std::size_t i = 0; i < validRangesCount; i++)
-		validRanges[i] = std::pair<uint8_t, uint8_t>(std::max(ranges[i].first, ranges[ranges.size() - i - 1].first), std::min(ranges[i].second, ranges[ranges.size() - i - 1].second));
+		validRanges[i] = DigitRange(std::max(ranges[i].min, ranges[ranges.size() - i - 1].min), std::min(ranges[i].max, ranges[ranges.size() - i - 1].max));
 
 	return validRanges;
 }
 
+
 template<typename ReturnType, typename U, typename Functor>
-constexpr void Palindromes::IteratePalindromes(ReturnType &number, U digit, U totalDigits, Functor&& callback)
+constexpr void IteratePalindromes(ReturnType &number, U digit, U totalDigits, Functor callback)
 {
     ReturnType diff = GetDiff<ReturnType>(digit, totalDigits);
 
@@ -242,32 +265,52 @@ constexpr void Palindromes::IteratePalindromes(ReturnType &number, U digit, U to
 }
 
 template<typename ReturnType, typename U, typename Functor>
-constexpr void Palindromes::IteratePalindromesRanging(ReturnType &number, U digit, U totalDigits, const ranges_t &validRanges, Functor&& callback)
+constexpr void IteratePalindromesRanging(ReturnType &number, U digit, U totalDigits, const std::vector<DigitRange> &validRanges, Functor callback)
 {
 	const DigitRange &validRange = validRanges[(totalDigits - digit) / U(2)];
 
     ReturnType diff = GetDiff<ReturnType>(digit, totalDigits);
 
-	if(digit == totalDigits && (validRange.first + 2) < validRange.second)
-	{
-		number = number + diff * std::max<uint8_t>(1, validRange.first);
-		callback(number);
-	}
+	if(digit == totalDigits)
+		number = number + diff * std::max<uint8_t>(1, validRange.min);
+	else
+		number = number + diff * validRange.min;
 
-	for(uint8_t i = validRange.first + 1 + (digit == totalDigits); i < validRange.second; i++)
+	for(uint8_t i = validRange.min + 1 + (digit == totalDigits); i < validRange.max; i++)
 	{
 		if(U(2) < digit)
+		{
 			IteratePalindromesRanging(number, digit - U(2), totalDigits, validRanges, callback);
+			number = number + diff;
+			continue;
+		}
 		
-		number = number + diff;
 		callback(number);
+		number = number + diff;
 	}
 
 	if(U(2) < digit)
 		IteratePalindromesRanging(number, digit - U(2), totalDigits, validRanges, callback);
+	else
+		callback(number);
 
-	number = number - diff * (validRange.second - validRange.first - 1 + (digit == totalDigits));
-}
+	number = number - diff * (validRange.max - 1);
 }
 
+
+template<typename T, typename U>
+#if __cplusplus >= 202002L
+constexpr
 #endif
+T GetDiff(U digit, U totalDigits)
+{
+	if constexpr(!std::is_arithmetic_v<T>)
+        return (FastPower10(digit) + T(1)) * (totalDigits - digit) / U(2);
+
+    return kGetDiffLookup<T>[digit] * FastPower10((totalDigits - digit) / U(2));
+}
+} // detail
+} // palindrome
+} // Tolik
+
+#endif // TOLIK_ALGORITHMS_PALINDROMES_HPP
